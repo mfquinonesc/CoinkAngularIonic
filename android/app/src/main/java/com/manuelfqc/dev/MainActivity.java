906b1e689f636a953aab8287a6d5be83c8b6428e
manuel <manuelfqc@gmail.com>
2024-02-19 18:52:49 -0500

    [19/02/2024 18:52] Done apk android

---
+++ android/app/src/main/java/com/manuelfqc/dev/MainActivity.java
@@ -0,0 +1,5 @@
+package com.manuelfqc.dev;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.manuelfqc.dev',
+  appName: 'CoinkAngularIonic',
+  webDir: 'dist/coink-angular-ionic',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
